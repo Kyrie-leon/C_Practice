70ab2b640875e0e8d20e218fc09ccb77edb0f9d9
Kyrie-leon <gaomeng9518@163.com>
2021-03-01 22:04:02 +0800

    this指针特性1

---
+++ day15/04-thisFeature1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class Date
+{
+public:
+  void Init(int year, int month, int day)
+  {
+    cout<<"this:"<<this<<endl;
+    this->_year = year;
+    this-> _month = month;
+    this-> _day = day;
+  }
+
+  void Print()
+  {
+    cout<<_year<<"-"<<_month<<"-"<<_day<<endl;
+  }
+
+private:
+  int _year;
+  int _month;
+  int _day;
+};
+
+int main()
+{
+  Date d1,d2;
+  d1.Init(2021,3,1);
+  d1.Print();
+  d2.Init(2021,2,28);
+  d2.Print();
+  return 0;
+}
